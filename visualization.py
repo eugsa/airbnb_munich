feacf301f6db877e59abb7bf0c6a999a50eca65f
eugsa <eugenie.saliou@gmail.com>
2024-05-21 15:49:30 +0200

    Refactor visualization and improve visualization parameters

--- visualization.py
+++ visualization.py
@@ -2,7 +2,12 @@ from config import *
 
 def listing_count_per_neighborhood_plot(df):
   count_per_neigh = df.groupby(by='neighborhood_name').agg({'id': 'count'}).rename(columns={'id': 'listing_count'}).reset_index()
-  print(count_per_neigh.sample(n=10))
-  plt.bar(count_per_neigh.neighborhood_name, count_per_neigh.listing_count)
-  plt.xticks(rotation=90)
+  plt.figure(figsize=(12, 6))
+  plt.bar(count_per_neigh.neighborhood_name, count_per_neigh.listing_count, zorder=2)
+  plt.title('Listing count per neighborhood plot')
+  plt.xlabel('Neihborhoods')
+  plt.ylabel('Number of listings')
+  plt.xticks(rotation=40, ha='right')
+  plt.subplots_adjust(bottom=0.3)
+  plt.grid(True, which='both', axis='y', linewidth=0.5, color='grey', zorder=0)
   plt.show()
